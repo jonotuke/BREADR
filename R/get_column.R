e80f3a2518cb2e80082278c822d6640b01b83aa4
Jonathan Tuke <jonotuke@gmail.com>
2024-07-11 20:05:16 +0930

    Temp documentation
    Added temp documentation to get_column function so check passes.

--- R/get_column.R
+++ R/get_column.R
@@ -1,16 +1,23 @@
 get_column_old <- function(genofile, col = 1){
   system(paste0('cut -c ',col,' ',genofile),intern=T)
 }
+#' get column
+#'
+#' @param genofile genofile
+#' @param col column to return
+#'
+#' @return column of numbers
+#' @export
 get_column_new <- function(genofile, col = 1){
-  read_lines(genofile) |>
-    map(\(x) stringr::str_sub(x, col, col)) |>
+  readr::read_lines(genofile) |>
+    purrr::map(\(x) stringr::str_sub(x, col, col)) |>
     unlist()
 }
-pacman::p_load(conflicted, tidyverse, targets, data.table)
-genofile <- system.file("extdata", "example.geno.txt", package = "BREADR")
-identical(get_column_old(genofile, 1), get_column_new(genofile, 1))
-
-microbenchmark::microbenchmark(
-  get_column_old(genofile, 1),
-  get_column_new(genofile, 1)
-)
+# pacman::p_load(conflicted, tidyverse, targets, data.table)
+# genofile <- system.file("extdata", "example.geno.txt", package = "BREADR")
+# identical(get_column_old(genofile, 1), get_column_new(genofile, 1))
+#
+# microbenchmark::microbenchmark(
+#   get_column_old(genofile, 1),
+#   get_column_new(genofile, 1)
+# )
